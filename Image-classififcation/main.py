e6d5886eeddfe23041a88c270d164f98e1a2931b
Mudit Gupta <muditg.world@gmail.com>
2022-09-26 19:00:56 +0530

    Fresh Repo

---
+++ Image-classififcation/main.py
@@ -0,0 +1,68 @@
+from pickletools import optimize
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+
+training_images, testing_images = training_images / 255.0, testing_images / 255.0
+
+class_names = ['Airplane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+model = models.load_model('image_classifier.model')
+
+def classify(file_path):
+    img = cv.imread(file_path)
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    img = cv.resize(img, (32, 32))
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    prediction = model.predict(np.array([img]) / 255.0)
+
+    index = np.argmax(prediction)
+    label.configure(foreground='#011638', text=class_names[index])
+    print(f'\n\nPrediction is : {class_names[index]}')
+
+
+top=tk.Tk()
+top.geometry('800x600')
+top.title('Image Classification')
+top.configure(background='#CDCDCD')
+label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
+sign_image = Label(top)
+
+def show_classify_button(file_path):
+    classify_b=Button(top,text="Classify Image",command=lambda: classify(file_path),padx=10,pady=5)
+    classify_b.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+    classify_b.place(relx=0.79,rely=0.46)
+    
+def upload_image(): 
+    try:
+        file_path=filedialog.askopenfilename()
+        uploaded=Image.open(file_path)
+        im=ImageTk.PhotoImage(uploaded)
+        
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+
+upload=Button(top,text="Upload an image",command=upload_image,padx=10,pady=5)
+upload.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+upload.pack(side=BOTTOM,pady=50)
+sign_image.pack(side=BOTTOM,expand=True)
+label.pack(side=BOTTOM,expand=True)
+
+heading = Label(top, text="Know Your Image",pady=20, font=('arial',20,'bold'))
+heading.configure(background='#CDCDCD',foreground='#364156')
+heading.pack()
+top.mainloop()
